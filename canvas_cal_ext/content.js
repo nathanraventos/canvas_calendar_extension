72decb4269372bb32ad46cd8d78ee1f9ecab7ce4
Nathan Raventos <rav21003@byui.edu>
2025-05-07 13:00:38 -0600

    Added icons folder and plank png files to project.

---
+++ canvas_cal_ext/content.js
@@ -0,0 +1,17 @@
+if (window.location.href.includes("calendar"))
+{
+    const button = document.createElement("button");
+    button.innerText = "Organize Calendar";
+
+    button.style.position = "fixed";
+    button.style.top = "10px";
+    button.style.right = "10px";
+    button.style.zIndex = "9999";
+    button.style.padding = "10px 15px";
+    button.style.backgroundColor = "#008CBA";
+    button.style.color = "#fff";
+    buttton.style.border = "none";
+    button.style.borderRadius = "5px";
+    button.style.cursor = "pointer";
+
+}
